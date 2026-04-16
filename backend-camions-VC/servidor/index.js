27a43c3aa5c6c31059a6d40030a3723689d95306
TudorRetegan <a2410044@institutmontilivi.cat>
2026-04-16 15:47:32 +0200

    node bienhecho

--- backend-camions-VC/servidor/index.js
+++ backend-camions-VC/servidor/index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "projectebigmags"
+    database: "projectebitmags"
 });
 
 db.connect(err => {
