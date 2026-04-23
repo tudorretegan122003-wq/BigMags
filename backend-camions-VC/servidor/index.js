6e4f7d2d1e9dc9f664eba874930299e5079a9499
Joel Gonzalez <a2410041@institutmontilivi.cat>
2026-04-23 15:26:48 +0200

    get camio id arreglat

--- backend-camions-VC/servidor/index.js
+++ backend-camions-VC/servidor/index.js
@@ -97,14 +97,18 @@ app.get("/trucks", (req, res) => {
 
 
 app.get("/trucks/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-    db.get(`SELECT * FROM trucks WHERE id = ${id}`, [], (err, row) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "El ID proporcionado no es un número válido" });
+    }
+
+    db.query("SELECT * FROM trucks WHERE id = ?", [id], (err, results) => {
         if (err) {
-            return res.status(500).json({error: "Error en la BD",descripcio:error});
+            return res.status(500).json({ error: "Error en la BD", descripcio: err });
         }
-        if (row) {
-            console.log(row)
-            return res.json(row);
+        if (results.length > 0) {
+            return res.json(results[0]);
         }
         else {
             return res.status(404).json({error: "Camión no encontrado"});
